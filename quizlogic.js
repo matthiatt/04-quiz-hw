19c4c88bca677a842037ec80840d6e1592a3678a
Matthew Hiatt <mhiatt62@gmail.com>
2020-11-14 23:20:35 -0600

    edits

--- quizlogic.js
+++ quizlogic.js
@@ -19,7 +19,6 @@ var buttonlbl2 = document.getElementById("lbl-button2");
 var buttonlbl3 = document.getElementById("lbl-button3");
 var buttonlbl4 = document.getElementById("lbl-button4");
 var initialsHighScore = document.getElementById("initialsHighScore"); // add in HTML
-var returnFeedback = document.getElementById("feedback"); // add in HTML
 var secondsLeft = 10;
 var questionTracker = 0;
 var allScores = [];
@@ -28,157 +27,158 @@ var score = 0;
 var currentQuestion = 0;
 var countDownTimer;
 var time = 60000;
+var timerId;
 // var time = myQuestions.length * 20;
-// var legnth = 
+// var legnth =
 var myQuestionsIndex = 0;
 
 // submitButton.addEventListener("click", showResults);
 
 var myQuestions = [
-    {
-      question: "What is the highest-grossing film of all time without taking inflation into account?",
-      answers: {
-        a: "Titanic",
-        b: "Avengers: Endgame",
-        c: "Star Wars: The Force Awakens",
-        d: "Avatar"
-      },
-      correctAnswer: "b"
+  {
+    question:
+      "What is the highest-grossing film of all time without taking inflation into account?",
+    answers: {
+      a: "Titanic",
+      b: "Avengers: Endgame",
+      c: "Star Wars: The Force Awakens",
+      d: "Avatar",
     },
-    {
-      question: "Which film did Steven Spielberg win his first Oscar for Best Director?",
-      answers: {
-        a: "Jaws",
-        b: "Catch Me If You Can",
-        c: "Schindler’s List",
-        d: "E.T."
-      },
-      correctAnswer: "c"
+    correctAnswer: "b",
+  },
+  {
+    question:
+      "Which film did Steven Spielberg win his first Oscar for Best Director?",
+    answers: {
+      a: "Jaws",
+      b: "Catch Me If You Can",
+      c: "Schindler’s List",
+      d: "E.T.",
     },
-    {
-      question: "What is the name of Quint’s shark-hunting boat in Jaws?",
-      answers: {
-        a: "The Whale",
-        b: "The Orca",
-        c: "The Shark",
-        d: "The Dolphin"
-      },
-      correctAnswer: "b"
+    correctAnswer: "c",
+  },
+  {
+    question: "What is the name of Quint’s shark-hunting boat in Jaws?",
+    answers: {
+      a: "The Whale",
+      b: "The Orca",
+      c: "The Shark",
+      d: "The Dolphin",
     },
-    {
-      question: "What short film featured Mickey Mouse’s first appearance?",
-      answers: {
-        a: "Plane Crazy",
-        b: "Wild Waves",
-        c: "The Band Concert",
-        d: "The Barnyard Concert"
-      },
-      correctAnswer: "a"
+    correctAnswer: "b",
+  },
+  {
+    question: "What short film featured Mickey Mouse’s first appearance?",
+    answers: {
+      a: "Plane Crazy",
+      b: "Wild Waves",
+      c: "The Band Concert",
+      d: "The Barnyard Concert",
     },
-    {
-      question: "As the Disney princess with the fewest lines, how many lines did Aurora (or “Sleeping Beauty”) have in total?",
-      answers: {
-        a: "11",
-        b: "15",
-        c: "18",
-        d: "20"
-      },
-      correctAnswer: "c"
-    }];
-
+    correctAnswer: "a",
+  },
+  {
+    question:
+      "As the Disney princess with the fewest lines, how many lines did Aurora (or “Sleeping Beauty”) have in total?",
+    answers: {
+      a: "11",
+      b: "15",
+      c: "18",
+      d: "20",
+    },
+    correctAnswer: "c",
+  },
+];
 
 //Next, I am showing HOW the list of questions will be displayed on the web-page.
 // Got help with tutor on this portion.
-// function quizBuild() 
+// function quizBuild()
 // {
 
-function startQuiz() 
-{
+function startQuiz() {
   questionBody.removeAttribute("class");
   timerId = setInterval(timingInterval, 1000);
   timerElement.textContent = time;
   getQuestion();
-  startButton.setAttribute("class", "hidden" );
+  startButton.setAttribute("class", "hidden");
 
   //TO DO: END GUIZ IF TIMER HITS 0 (ZERO);
 }
 // var output = [];
 // var questionNumber = 0;
 
-function getQuestion() 
-{
+function getQuestion() {
   var cQuestion = myQuestions[myQuestionsIndex];
-  
+
   buttonlbl1.innerText = cQuestion.answers.a;
   buttonlbl2.innerText = cQuestion.answers.b;
   buttonlbl3.innerText = cQuestion.answers.c;
   buttonlbl4.innerText = cQuestion.answers.d;
-  
+
   questionsElement.textContent = cQuestion.question;
 }
 
-function questionClick() 
-{   
-  if (this.value !== myQuestions[myQuestionsIndex].answer) 
-  {
+function questionClick() {
+  var userAnswer = getAnswer();
+
+  if (userAnswer !== myQuestions[myQuestionsIndex].answer) {
     time -= 15;
-    if (time < 0) 
-    {time = 0;
+    if (time < 0) {
+      time = 0;
     }
     timerElement.textContent = time;
-  
-    returnFeedback.setAttribute("class", "feedback");
-  setTimeout(function() 
-  {
-    returnFeedback.setAttribute("class", "feedback hide");
-  }, 1000);
+  } else {
+    score++;
+  }
 
-  myQuestionIndex ++;
+  myQuestionsIndex++;
 
-  if(myQuestionIndex === myQuestions.length)
-  {
+  if (myQuestionsIndex === myQuestions.length) {
     quizEnd();
-  }else
-  {
+  } else {
     getQuestion();
   }
-  }}
-
-  function quizEnd()
-  {
-    clearTime(setTime);
-
-    var completeScreen = document.getElementById("endResult");
-  completeScreen.removeAttribute("class");
-
-    var finalScoreScreen = document.getElementById("finalScore");
-  finalScoreScreen.textContent = time;
-
-    // questionsElement.setAttribute("class", "hide");
 }
 
-function timingInterval() 
-{
+function getAnswer() {
+  var radios = document.getElementsByName("btnChoice");
+
+  for (var i = 0, length = radios.length; i < length; i++) {
+    if (radios[i].checked) {
+      return radios[i].value;
+    }
+  }
+
+  return "";
+}
+
+function quizEnd() {
+  clearInterval(timerId);
+
+  var finalScoreScreen = document.getElementById("finalScore");
+  finalScoreScreen.textContent = score;
+
+  questionBody.setAttribute("class", "hidden");
+  submitButton.removeAttribute("class");
+}
+
+function timingInterval() {
   time--;
   timerElement.textContent = time;
-  if (time <= 0) 
-  {
+  if (time <= 0) {
     quizEnd();
   }
 }
 
-function saveHighscore() 
-{
+function saveHighscore() {
   var initials = initialsHighScore;
-  if (initials !== "") 
-  {
+  if (initials !== "") {
     var highscores =
       JSON.parse(window.localStorage.getItem("highscores")) || [];
 
-    var newScore = 
-    {
+    var newScore = {
       score: time,
-      initials: initials
+      initials: initials,
     };
 
     highscores.push(newScore);
@@ -188,16 +188,14 @@ function saveHighscore()
   // button.addEventListener('click', saveHighscore);
 }
 
-function check(event) 
-{
-  if (event.keyEvent === "Enter") 
-  {
+function check(event) {
+  if (event.keyEvent === "Enter") {
     saveHighscore();
   }
 }
 
 // initialsEl.onkeyup = check;
-    // display new time on page    timerElement.textContent = time;
+// display new time on page    timerElement.textContent = time;
 //   function nextQuestion() {
 //   for (i = 0; i < myQuestions.length; i++) {
 //     body.innerHTML = "<p>" + myQuestions[currentQuestion].question + "</p>";
@@ -223,52 +221,51 @@ function check(event)
 // }
 // renderQuestion();
 
+// var answers = [];
+// for (var letter in currentQuestion.answers)
+//     {
+//         answers.push(
+//             `<label>
+//                 <input type="radio" name="questions${questionNumber}" value = "${letter}">${letter}</input> :
+//                 ${currentQuestion.answers[letter]}
+//             </label>`
+//         );
+//     }
 
-        // var answers = [];
-        // for (var letter in currentQuestion.answers) 
-    //     {
-    //         answers.push( 
-    //             `<label>
-    //                 <input type="radio" name="questions${questionNumber}" value = "${letter}">${letter}</input> :
-    //                 ${currentQuestion.answers[letter]}
-    //             </label>`
-    //         );
-    //     }
-
-    //     output.push(
-    //         `<div id="quizAnswerAndQuestion${questionNumber}" style = "display: none">
-    //         <div class="question"> ${currentQuestion.question} </div>
-    //         <div class="answers"> ${answers.join('')} </div>
-    //         </div>`
-    //       ); questionNumber++;
-    // });
-    //   document.getElementById("quizAnswerAndQuestion" + questionTracker ).style.display = "none";
-    //   questionTracker++;
-    //   document.getElementById("quizAnswerAndQuestion" + questionTracker ).style.display = "block";
-      // questionTracker.foreachQuestion(myQuestions);
-      // output.push(
-      //       `<div id="quizAnswerAndQuestion${questionNumber}" style = "display: none">`
-      // var q1 = document.getElementById("question1").innerHTML += index;
-      //Use question tracker to hide current <div id="quizAnswerAndQuestion${questionNumber}">
-      // incriment questionTracker by 1
-      // use questionTracker to show new current div
-      // ); questionTracker++;
-    // }
+//     output.push(
+//         `<div id="quizAnswerAndQuestion${questionNumber}" style = "display: none">
+//         <div class="question"> ${currentQuestion.question} </div>
+//         <div class="answers"> ${answers.join('')} </div>
+//         </div>`
+//       ); questionNumber++;
+// });
+//   document.getElementById("quizAnswerAndQuestion" + questionTracker ).style.display = "none";
+//   questionTracker++;
+//   document.getElementById("quizAnswerAndQuestion" + questionTracker ).style.display = "block";
+// questionTracker.foreachQuestion(myQuestions);
+// output.push(
+//       `<div id="quizAnswerAndQuestion${questionNumber}" style = "display: none">`
+// var q1 = document.getElementById("question1").innerHTML += index;
+//Use question tracker to hide current <div id="quizAnswerAndQuestion${questionNumber}">
+// incriment questionTracker by 1
+// use questionTracker to show new current div
+// ); questionTracker++;
+// }
 
 //Next, since I generated the whole HTML concept for each question, I am going to join everything together and display it on the page.
 // quizContainer.innerHTML = output.join('');
 
 //Now I need to be able to show my results based on the function loop and check them.
 
-// function showResults() 
+// function showResults()
 // {
 //     var answerContainers = quizContainer.querySelectorAll('.answers');
 //     var numCorrect = 0;
 //     for (var i = 0; i < myQuestions.length; i++);
-  
-//     function myQuestions(currentQuestion, questionNumber) 
+
+//     function myQuestions(currentQuestion, questionNumber)
 //     {
-    
+
 //       var selector = `input[name='questions${questionNumber}']:checked`;
 //       var userAnswer = document.querySelector(selector);
 //       if(userAnswer.value === currentQuestion.correctAnswer)
@@ -290,15 +287,15 @@ function check(event)
 
 // Ask tutor why this is not working.
 
-// function setTime() 
+// function setTime()
 // {
 
-//   var timerInterval = setInterval(function() 
+//   var timerInterval = setInterval(function()
 //   {
 //     secondsLeft--;
 //     timeEl.textContent = secondsLeft + " seconds left to answer the question.";
 
-//     if(secondsLeft === 0) 
+//     if(secondsLeft === 0)
 //     {
 //       clearTime(timerInterval);
 //       sendMessage();
@@ -307,7 +304,7 @@ function check(event)
 //   }, 1000);
 // }
 
-// function sendMessage() 
+// function sendMessage()
 // {
 //   timeEl.textContent = "Time is up, try again!";
 
